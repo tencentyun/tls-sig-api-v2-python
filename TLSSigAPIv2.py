2427b4d8972a1e28ced05a26b5fa2c59031d169c
yutingzeng <yutingzeng@tencent.com>
2020-08-03 17:20:06 +0800

    修改函数名和注释

--- TLSSigAPIv2.py
+++ TLSSigAPIv2.py
@@ -135,26 +135,52 @@ class TLSSigAPIv2:
         base64_sig = base64_encode_url(sig_cmpressed)
         return base64_sig
 
-    def gen_sig(self, identifier, expire=180*86400):
+    ##
+    #【功能说明】用于签发 TRTC 和 IM 服务中必须要使用的 UserSig 鉴权票据
+    #
+    #【参数说明】
+    # userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
+    # expire - UserSig 票据的过期时间，单位是秒，比如 86400 代表生成的 UserSig 票据在一天后就无法再使用了。
+    #/
+    def genUserSig(self, userid, expire=180*86400):
         """ 用户可以采用默认的有效期生成 sig """
-        return self.__gen_sig(identifier, expire, None)
-    # @brief 生成带userbuf的sig
-    # @param identifier 用户名
-    # @param roomnum  数字房间号
-    # @param expire 过期时间：该权限加密串的过期时间，实际过期时间：当前时间+expire
-    # @param privilege 用户权限，255表示所有权限
-    def gen_sig_with_userbuf(self, identifier, expire, roomnum, privilege):
+        return self.__gen_sig(userid, expire, None)
+    ##
+    #【功能说明】
+    # 用于签发 TRTC 进房参数中可选的 PrivateMapKey 权限票据。
+    # PrivateMapKey  需要跟 UserSig 一起使用，但比 UserSig 有更强的权限控制能力：
+    # - UserSig 只能控制某个 UserID 有无使用 TRTC 服务的权限，只要 UserSig 正确，其对应的 UserID 可以进出任意房间。
+    # - PrivateMapKey 则是将 UserID 的权限控制的更加严格，包括能不能进入某个房间，能不能在该房间里上行音视频等等。
+    # 如果要开启 PrivateMapKey 严格权限位校验，需要在【实时音视频控制台】=>【应用管理】=>【应用信息】中“启动权限密钥”。
+    # 
+    #【参数说明】
+    # userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
+    # expire - PrivateMapKey 票据的过期时间，单位是秒，比如 86400 生成的 PrivateMapKey 票据在一天后就无法再使用了。
+    # roomid - 房间号，用于指定该 userid 可以进入的房间号
+    # privilegeMap - 权限位，使用了一个字节中的 8 个比特位，分别代表八个具体的功能权限：
+    #  - 第 1 位：0000 0001 = 1，创建房间的权限
+    #  - 第 2 位：0000 0010 = 2，加入房间的权限
+    #  - 第 3 位：0000 0100 = 4，发送语音的权限
+    #  - 第 4 位：0000 1000 = 8，接收语音的权限
+    #  - 第 5 位：0001 0000 = 16，发送视频的权限  
+    #  - 第 6 位：0010 0000 = 32，接收视频的权限  
+    #  - 第 7 位：0100 0000 = 64，发送辅路（也就是屏幕分享）视频的权限
+    #  - 第 8 位：1000 0000 = 200，接收辅路（也就是屏幕分享）视频的权限  
+    #  - privilegeMap == 1111 1111 == 255 代表该 userid 在该 roomid 房间内的所有功能权限。
+    #  - privilegeMap == 0010 1010 == 42  代表该 userid 拥有加入房间和接收音视频数据的权限，但不具备其他权限。
+    #/
+    def genPrivateMapKey(self, userid, expire, roomid, privilegeMap):
         """ 带 userbuf 生成签名 """
-        userbuf = self._gen_userbuf("xiaojun",roomnum,expire,privilege,0)
+        userbuf = self._gen_userbuf(userid,roomid,expire,privilegeMap,0)
         print(userbuf)
-        return self.__gen_sig(identifier, expire, userbuf)
+        return self.__gen_sig(userid, expire, userbuf)
 
 
 def main():
     api = TLSSigAPIv2(1400000000, '5bd2850fff3ecb11d7c805251c51ee463a25727bddc2385f3fa8bfee1bb93b5e')
-    sig = api.gen_sig("xiaojun")
+    sig = api.genUserSig("xiaojun")
     print(sig)
-    sig = api.gen_sig_with_userbuf("xiaojun", 86400*180,10000,255)
+    sig = api.genPrivateMapKey("xiaojun", 86400*180,10000,255)
     print(sig)
 
 
